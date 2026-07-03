284af7b12e3d6f4998d8d29f122905cdaa406d5c
Braulio Banegas <braulio.banegasb@gmail.com>
2026-07-02 21:42:01 -0300

    Clase 1 - Fundamentos de JavaScript

---
+++ app.js
@@ -0,0 +1,25 @@
+let publicaciones = [
+    {
+        titulo: "Soporte para cortina",
+        estado: "Nuevo",
+        busca: "Taladro"
+    },
+    {
+        titulo: "Bicicleta Venzo",
+        estado: "Usada",
+        busca: "Notebook"
+    },
+    {
+        titulo: "Amoladora",
+        estado: "Usada",
+        busca: "Soldadora"
+    },
+    {
+        titulo: "salamandra",
+        estado: "Usada",
+        busca: "tromen"
+
+    }
+];  
+
+console.log(publicaciones[3].titulo);
