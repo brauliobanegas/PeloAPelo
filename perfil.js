906568e023609f869b8debe6c829738be9efd991
Braulio Banegas <braulio.banegasb@gmail.com>
2026-07-11 14:21:28 -0300

    Carga datos del usuario en perfil desde Supabase

--- perfil.js
+++ perfil.js
@@ -9,7 +9,34 @@
 
     }
 
+    const usuarioAuth = data.session.user;
+
+
     document.getElementById("emailUsuario").textContent =
-        data.session.user.email;
+        usuarioAuth.email;
+
+
+    const { data: usuario, error } = await supabaseClient
+        .from("usuarios")
+        .select("*")
+        .eq("auth_id", usuarioAuth.id)
+        .single();
+
+
+    if (error) {
+
+        console.error("Error obteniendo perfil:", error);
+        return;
+
+    }
+
+
+    document.getElementById("nombreUsuario").textContent =
+        usuario.nombre;
+
+
+    document.getElementById("telefonoUsuario").textContent =
+        usuario.telefono || "No cargado";
+
 
 })();
